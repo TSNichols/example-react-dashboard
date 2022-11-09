1a35d8e74008240503495229e7027d3ada9a9f08
Talcott Nichols <talcott.nichols@gmail.com>
2022-11-09 13:45:36 -0500

    add gh-pages for deployment

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,5 @@
 import { createContext, useState, useMemo } from "react"
 import { createTheme } from "@mui/material/styles"
-import { SportsRugbySharp } from "@mui/icons-material"
 
 // color design tokens
 const colors = {
